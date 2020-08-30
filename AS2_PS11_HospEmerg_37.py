974a33016ee21dd57431b1ba11378e82dc72cae0
Anjali Sunder Naik (RBEI/ETL) <SunderNaik.Anjali@in.bosch.com>
2020-08-30 16:58:01 +0530

    fixed the bugs

--- AS2_PS11_HospEmerg_37.py
+++ AS2_PS11_HospEmerg_37.py
@@ -9,148 +9,79 @@ Created on Sat August 27 2020
     Pranesh P	2019HC04175
     Anjali Sunder Naik	2019HC04178
 """
-import sys
-class Vertex:
-    def __init__(self, node):
-        self.id = node
-        self.adjacent = {}
-        # Set distance to infinity for all nodes
-        self.distance = sys.maxint
-        # Mark all nodes unvisited        
-        self.visited = False  
-        # Predecessor
-        self.previous = None
 
-    def add_neighbor(self, neighbor, weight=0):
-        self.adjacent[neighbor] = weight
+from collections import defaultdict
 
-    def get_connections(self):
-        return self.adjacent.keys()  
-
-    def get_id(self):
-        return self.id
-
-    def get_weight(self, neighbor):
-        return self.adjacent[neighbor]
-
-    def set_distance(self, dist):
-        self.distance = dist
-
-    def get_distance(self):
-        return self.distance
-
-    def set_previous(self, prev):
-        self.previous = prev
-
-    def set_visited(self):
-        self.visited = True
-
-    def __str__(self):
-        return str(self.id) + ' adjacent: ' + str([x.id for x in self.adjacent])
-
-class Graph:
+class Graph():
     def __init__(self):
-        self.vert_dict = {}
-        self.num_vertices = 0
-
-    def __iter__(self):
-        return iter(self.vert_dict.values())
-
-    def add_vertex(self, node):
-        self.num_vertices = self.num_vertices + 1
-        new_vertex = Vertex(node)
-        self.vert_dict[node] = new_vertex
-        return new_vertex
-
-    def get_vertex(self, n):
-        if n in self.vert_dict:
-            return self.vert_dict[n]
-        else:
-            return None
-
-    def add_edge(self, frm, to, cost = 0):
-        if frm not in self.vert_dict:
-            self.add_vertex(frm)
-        if to not in self.vert_dict:
-            self.add_vertex(to)
-
-        self.vert_dict[frm].add_neighbor(self.vert_dict[to], cost)
-        self.vert_dict[to].add_neighbor(self.vert_dict[frm], cost)
-
-    def get_vertices(self):
-        return self.vert_dict.keys()
-
-    def set_previous(self, current):
-        self.previous = current
-
-    def get_previous(self, current):
-        return self.previous
-
-def shortest(v, path):
-    ''' make shortest path from v.previous'''
-    if v.previous:
-        path.append(v.previous.get_id())
-        shortest(v.previous, path)
-    return
-
-import heapq
-
-def dijkstra(aGraph, start):
-    print '''Dijkstra's shortest path'''
-    # Set the distance for the start node to zero 
-    start.set_distance(0)
-
-    # Put tuple pair into the priority queue
-    unvisited_queue = [(v.get_distance(),v) for v in aGraph]
+        """
+        self.edges is a dict of all possible next nodes
+        e.g. {'X': ['A', 'B', 'C', 'E'], ...}
+        self.weights has all the weights between two nodes,
+        with the two nodes as a tuple as the key
+        e.g. {('X', 'A'): 7, ('X', 'B'): 2, ...}
+        """
+        self.edges = defaultdict(list)
+        self.weights = {}
     
-    heapq.heapify(unvisited_queue)
+    def add_edge(self, from_node, to_node, weight):
+        # Note: assumes edges are bi-directional
+        self.edges[from_node].append(to_node)
+        self.edges[to_node].append(from_node)
+        self.weights[(from_node, to_node)] = weight
+        self.weights[(to_node, from_node)] = weight
 
-    while len(unvisited_queue):
-        # Pops a vertex with the smallest distance 
-        uv = heapq.heappop(unvisited_queue)
-        current = uv[1]
-        current.set_visited()
+    def get_weight(self,from_node, to_node):
+        weight= None
+        for key, value in self.weights.items():
+            if (key[0]==from_node and key[1]==to_node):
+                weight= value
+                break
+        return weight    
 
-        #for next in v.adjacent:
-        for next in current.adjacent:
-            # if visited, skip
-            if next.visited:
-                continue
-            new_dist = current.get_distance() + current.get_weight(next)
-            
-            if new_dist < next.get_distance():
-                next.set_distance(new_dist)
-                next.set_previous(current)
-                print 'updated : current = %s next = %s new_dist = %s' \
-                        %(current.get_id(), next.get_id(), next.get_distance())
+
+def dijsktra(graph, initial, end):
+    # shortest paths is a dict of nodes
+    # whose value is a tuple of (previous node, weight)
+    shortest_paths = {initial: (None, 0)}
+    current_node = initial
+    visited = set()
+    
+    while current_node != end:
+        visited.add(current_node)
+        destinations = graph.edges[current_node]
+        weight_to_current_node = shortest_paths[current_node][1]
+
+        for next_node in destinations:
+            weight = graph.weights[(current_node, next_node)] + weight_to_current_node
+            if next_node not in shortest_paths:
+                shortest_paths[next_node] = (current_node, weight)
             else:
-                print 'not updated : current = %s next = %s new_dist = %s' \
-                        %(current.get_id(), next.get_id(), next.get_distance())
-        # Rebuild heap
-        # 1. Pop every item
-        while len(unvisited_queue):
-            heapq.heappop(unvisited_queue)
-        # 2. Put all vertices not visited into the queue
-        unvisited_queue = [(v.get_distance(),v) for v in aGraph if not v.visited]
-        heapq.heapify(unvisited_queue)
+                current_shortest_weight = shortest_paths[next_node][1]
+                if current_shortest_weight > weight:
+                    shortest_paths[next_node] = (current_node, weight)
+        
+        next_destinations = {node: shortest_paths[node] for node in shortest_paths if node not in visited}
+        if not next_destinations:
+            return "Route Not Possible"
+        # next node is the destination with the lowest weight
+        current_node = min(next_destinations, key=lambda k: next_destinations[k][1])
+    
+    # Work back through destinations in shortest path
+    path = []
+    while current_node is not None:
+        path.append(current_node)
+        next_node = shortest_paths[current_node][0]
+        current_node = next_node
+    # Reverse path
+    path = path[::-1]
+    return path
 
 class HospitalEmergency:
 
     # This function is called to get the shortest route
-    def get_shortest_path(self,g):
-        print ('Graph data:')
-        for v in g:
-            for w in v.get_connections():
-                vid = v.get_id()
-                wid = w.get_id()
-                print ('( %s , %s, %3d)'  % ( vid, wid, v.get_weight(w)))    
-        dijkstra(g, g.get_vertex(str(hospital_node.strip()))) 
-        target = g.get_vertex(str(airport_node.strip()))
-        path = [target.get_id()]
-        shortest(target, path)
-        shortest_path=path[::-1];
-        print ('The shortest path : %s' %(path[::-1]))
-        return shortest_path
+    def get_shortest_path(self,g,from_node,to_node):
+        return dijsktra(g, from_node,to_node)
 
     # This function is called to get the time taken for the ambulance to reach the airport
     def get_time_taken_to_reach_airport(self, distance):
@@ -167,33 +98,20 @@ class HospitalEmergency:
         shortest_path_sequence.pop(0)   
         return shortest_path_sequence
 
-    # This is a helper function which returns the weight between 2 nodes
-    def get_distance_between_nodes(self,from_node, to_node):
-        distance=None;
-        for v in g:
-            for w in v.get_connections():
-                vid = v.get_id()
-                wid = w.get_id()
-                if(vid == from_node and wid == to_node):
-                    distance= v.get_weight(w)
-                    break  
-        return distance  
-
     # This function returns the shortest distance in kms, which the ambulance has to travel from hospital to airport.
     def get_shortest_distance(self, shortest_path):
         shortest_distance=0
         shortest_path_sequence= self.get_path_sequence(shortest_path)
         for i in shortest_path_sequence:
-            distance_between_nodes =self.get_distance_between_nodes(i[0],i[1])
+            distance_between_nodes =g.get_weight(i[0],i[1])
             shortest_distance += distance_between_nodes
         return shortest_distance
 
-if __name__ == '__main__':
+if __name__ == '__main__':    
     hospitalEmergency = None
     inputs = open('inputPS11.txt', 'r')
     output = open('outputPS11.txt', 'w')
     STRING_CONCAT = "Shortest route from the hospital '%s' to reach the airport '%s' is %s\nand it has minimum travel distance %skm\nit will take %s minutes for the ambulance to reach the airport."
-    vertices = []
     edges =[]
     for i in inputs:
         if 'Hospital Node' in i:
@@ -202,19 +120,15 @@ if __name__ == '__main__':
             itype, airport_node = i.split(':')
         if '/' in i:
             edge = i.strip('\n').split('/')
-            if edge[0].strip() not in vertices:
-                vertices.append(edge[0].strip())
-            if edge[1].strip() not in vertices:
-                vertices.append(edge[1].strip())
-            edges.append(edge)
+            input_edge= (edge[0].strip(),edge[1].strip(),int(edge[2].strip()))
+            edges.append(input_edge)
     g = Graph() 
-    for i in vertices:
-        g.add_vertex(i)  
-    for j in edges:
-        g.add_edge(j[0].strip(),j[1].strip(),int(j[2].strip()))      
+    for edge in edges:
+        g.add_edge(*edge)
 
     hospitalEmergency = HospitalEmergency()
-    shortest_path = hospitalEmergency.get_shortest_path(g)
-    min_distance = str(hospitalEmergency.get_shortest_distance(shortest_path))
+    shortest_path = hospitalEmergency.get_shortest_path(g, str(hospital_node.strip()),str(airport_node.strip()))
+    min_distance = hospitalEmergency.get_shortest_distance(shortest_path)
     time_taken =hospitalEmergency.get_time_taken_to_reach_airport(min_distance)
-    output.write(STRING_CONCAT % (hospital_node.strip(),airport_node.strip(),shortest_path,min_distance,time_taken)) 
+    output.write(STRING_CONCAT % (hospital_node.strip(),airport_node.strip(),shortest_path,min_distance,time_taken))     
+ 
